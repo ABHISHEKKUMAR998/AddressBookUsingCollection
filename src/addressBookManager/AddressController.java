bfcbca3fcfc0e0044b9d2d046d2a636b90c4f8ac
Abhishek <ABHISHEKKUMAR998>
2021-04-30 21:39:28 +0530

    Address book using Collections

---
+++ src/addressBookManager/AddressController.java
@@ -0,0 +1,89 @@
+package addressBookManager;
+import java.util.List;
+import java.util.Scanner;
+public class AddressController {
+	IAddressBookService addressBookService = new AdressService();
+	Contact	contact;
+	Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		new AddressController().userInput();
+
+
+	}
+
+	public void userInput() {
+		System.out.println("Select the option to be performed:  Enter 1 to ADD the CONTACT, Enter 2 to EDIT, Enter 3 to DELETE the CONTACT, Enter 4 to EXIT ,Enter 5 to   ");
+		int option =scanner.nextInt();
+		switch (option)
+		{
+		case 1:
+			scanner.nextLine();
+			System.out.println("\t\t\t Enter the DETAILS");
+			System.out.println("Enter the FirstName");
+			String first_Name = scanner.next();
+
+			System.out.println("Enter the LastName");
+
+			String last_Name = scanner.next();
+
+			System.out.println("Enter the Address");
+
+			String address = scanner.next();
+
+			System.out.println("Enter the City");
+
+			String city = scanner.next();
+
+			System.out.println("Enter the PinCode");
+
+			String pincode = scanner.next();
+
+			System.out.println("Enter the PhoneNo");
+
+			String phoneNo = scanner.next();
+
+			System.out.println("Enter the Email");
+			String email = scanner.next();
+
+			contact = new Contact(first_Name,last_Name,address,city,pincode,phoneNo,email);
+			addressBookService.addContact(contact);
+			userInput();
+			break;
+
+
+		case 2:
+
+			System.out.println("Enter the name ");
+			String contactName = scanner.next();
+			addressBookService.editContact(contactName,scanner);
+			userInput();
+			break;
+
+		case 3:
+
+			System.out.println("Enter the name ");
+			String contactName1 = scanner.next();
+
+
+			addressBookService.deleteContact(contactName1);
+
+			userInput();
+			break;
+
+		case 4:
+			exit();
+		case 5:
+			List<Contact> contactlist=addressBookService.showData();
+			for (int i = 0; i < contactlist.size(); i++) {
+				System.out.println(contactlist);
+			}
+			userInput();
+			break;
+		default :
+			System.out.println("Invalid Entry");
+		}
+	}
+	private void exit() {
+		System.exit(0);
+	}
+}
---
+++ src/addressBookManager/AdressService.java
@@ -0,0 +1,93 @@
+package addressBookManager;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public  class AdressService implements IAddressBookService {
+
+	public List<Contact> list ;
+
+	public AdressService() {
+		list = new ArrayList<Contact>();
+
+	}       
+
+	@Override
+	public boolean addContact(Contact contact) {
+
+
+		list.add(contact);
+
+
+		return false;
+	}
+
+
+
+
+	public boolean deleteContact(String contactName1) {
+		for (int i = 0; i < list.size(); i++) {
+			if(contactName1.equalsIgnoreCase(list.get(i).first_Name));
+			list.remove(i);
+			return true;
+		}
+
+
+		return false;
+	}
+
+
+
+	@Override
+	public  List<Contact> showData() {
+
+		return list;
+
+	}
+
+	public boolean editContact(String contactName, Scanner scanner) {
+		for (int i = 0; i < list.size(); i++) {
+
+
+			if(list.get(i).first_Name.equalsIgnoreCase(contactName))
+
+			{
+				System.out.println("Enter the FirstName");
+				String first_Name = scanner.next();
+
+				System.out.println("Enter the LastName");
+				String last_Name = scanner.next();
+
+				System.out.println("Enter the Address");
+				String address = scanner.next();
+
+				System.out.println("Enter the City");
+				String city = scanner.next();
+
+				System.out.println("Enter the PinCode");
+				String pincode = scanner.next();
+
+				System.out.println("Enter the PhoneNo");
+				String phoneNo = scanner.next();
+
+				System.out.println("Enter the Email");
+				String email = scanner.next();
+				list.get(i).setFirst_Name(first_Name);
+				list.get(i).setLast_Name(last_Name);
+				list.get(i).setCity(city);
+				list.get(i).setAddress(address);
+				list.get(i).setPhoneNo(phoneNo);
+				list.get(i).setPincode(pincode);
+				list.get(i).setEmail(email);
+				System.out.println("Updated list" + list.get(i));
+			}
+
+
+		}
+
+		return true;
+
+	}
+
+}
